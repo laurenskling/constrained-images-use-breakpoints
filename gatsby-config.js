fb1afbf304dcc639eb5c1974a8824e554ff004ac
Laurens Kling <laurens@goedideemedia.nl>
2021-08-19 22:27:27 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,24 @@
 module.exports = {
+  flags: {
+    LMDB_STORE: true,
+    PARALLEL_QUERY_RUNNING: true,
+  },
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Gatsby Site",
   },
-  plugins: [],
+  plugins: [
+    // Simple config, passing URL
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "SWAPI",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "swapi",
+        // Url to query from
+        url: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+      },
+    },
+  ],
 };
